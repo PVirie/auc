a4efa26e88144d6f55395a18e4f6d75296f72b93
Tao <tao@thebitstudio.com>
2017-07-24 12:14:08 +0700

    implement reprojection

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ mnist = input_data.read_data_sets("data/", one_hot=True)
 parser = argparse.ArgumentParser()
 parser.add_argument("--load", help="load weight", action="store_true")
 parser.add_argument("--coeff", help="update rate", type=float)
+parser.add_argument("--eval", help="evaluation coefficient", type=float)
 parser.add_argument("--rate", help="learning rate", type=float)
-parser.add_argument("--skip", help="example skip", type=int)
-parser.add_argument("--limit", help="example limit", type=int)
+parser.add_argument("--skip", help="run skip", type=int)
+parser.add_argument("--limit", help="run limit", type=int)
+parser.add_argument("--boot_skip", help="bootstrap skip", type=int)
+parser.add_argument("--boot_limit", help="bootstrap limit", type=int)
 parser.add_argument("--infer", help="total inference steps", type=int)
 args = parser.parse_args()
 
@@ -26,12 +29,15 @@ if __name__ == '__main__':
     data = np.concatenate([mnist.train.images, mnist.test.images, mnist.validation.images], axis=0)
     labels = np.concatenate([mnist.train.labels, mnist.test.labels, mnist.validation.labels], axis=0)
 
-    layer_sizes = [400, 400, 200, 100]
-    # layer_sizes = [400, 200, 100, 50, 20, 10]
+    layer_sizes = [400, 200, 100, 50, 20]
+    # layer_sizes = [400, 100]
     learning_coeff = 0.01 if not args.coeff else args.coeff
+    eval_coeff = 0.01 if not args.eval else args.eval
     learning_rate = 0.01 if not args.rate else args.rate
-    run_skip = 0 if not args.skip else args.skip
+    run_skip = 1000 if not args.skip else args.skip
     run_limit = 1000 if not args.limit else args.limit
+    bootstrap_skip = 0 if not args.skip else args.boot_skip
+    bootstrap_limit = 1000 if not args.limit else args.boot_limit
     infer_steps = 100 if not args.infer else args.infer
 
     print "-----------------------"
@@ -39,54 +45,67 @@ if __name__ == '__main__':
     print "Labels:", labels.shape
     print "layer sizes: ", layer_sizes
     print "learning coeff: ", learning_coeff
+    print "evaluation coeff: ", eval_coeff
     print "learning rate: ", learning_rate
     print "run skip: ", run_skip
     print "run limit: ", run_limit
+    print "bootstrap skip: ", bootstrap_skip
+    print "bootstrap limit: ", bootstrap_limit
     print "inference steps: ", infer_steps
     print "-----------------------"
 
     sess = tf.Session()
-    model = matter.Autoencoder(sess, (data.shape[1], labels.shape[1]), layer_sizes, learning_coeff, learning_rate)
+    model = matter.Autoencoder(sess, (data.shape[1], labels.shape[1]), layer_sizes, learning_rate)
     sess.run(tf.global_variables_initializer())
 
     if args.load:
         print "loading..."
         model.load()
 
+    for i in xrange(bootstrap_skip, bootstrap_skip + bootstrap_limit, 1):
+        model.collect(data[i:i + 1, :], labels[i:i + 1, :], learning_coeff)
+
+    for j in xrange(2000):
+        model.learn()
+
     error_graph = []
     average_error = 1.0
     for i in xrange(run_skip, run_skip + run_limit, 1):
 
-        model.reset_labels(data[i:i + 1, :])
-
+        label_ = np.zeros((1, labels.shape[1]))
         projected_ = data[i:i + 1, :]
-        for j in xrange(infer_steps):
-            label_, projected_, _ = model.infer(data[i:i + 1, :])
+        model.reset_input(projected_, label_)
+        for j in xrange(10):
+            model.reset_info(projected_, label_)
+            for k in xrange(10):
+                label_, projected_, _ = model.infer()
         print _
-
-        model.collect(data[i:i + 1, :], labels[i:i + 1, :])
-
-        for j in xrange(infer_steps):
-            model.learn(data[i:i + 1, :], labels[i:i + 1, :])
 
         gtruth = np.argmax(labels[i, :])
         predicted = np.argmax(label_)
         print gtruth, predicted
+
         if predicted == gtruth:
-            average_error = (1 - learning_coeff) * average_error
+            average_error = (1 - eval_coeff) * average_error
+            model.collect(data[i:i + 1, :], labels[i:i + 1, :], learning_coeff)
         else:
-            average_error = learning_coeff + (1 - learning_coeff) * average_error
+            average_error = eval_coeff + (1 - eval_coeff) * average_error
+            model.collect(data[i:i + 1, :], labels[i:i + 1, :], learning_coeff * 2)
         # average_error = learning_coeff * (np.sum((label_ - labels[i, :])**2)) + (1 - learning_coeff) * average_error
+
         print average_error
         error_graph.append(average_error)
-        # model.debug_test()
-
-        if i % 100 == 0:
-            model.save()
 
         canvas = np.concatenate((np.reshape(data[i:i + 1, :], (28, 28)), np.reshape(projected_, (28, 28))), axis=1)
         cv2.imshow("a", canvas)
         cv2.waitKey(1)
+
+        for j in xrange(infer_steps):
+            model.learn()
+        # model.debug_test()
+        if i % 100 == 0:
+            model.save()
+
     model.save()
 
     plt.plot(xrange(run_skip, run_skip + run_limit, 1), error_graph)
--- src/matter.py
+++ src/matter.py
@@ -8,9 +8,8 @@ def mirroring_relus(input):
 
 
 class Layer:
-    def __init__(self, shape, learning_coeff, learning_rate=0.01):
+    def __init__(self, shape, learning_rate=0.01):
         self.shape = shape
-        self.learning_coeff = learning_coeff
         self.learning_rate = learning_rate
         self.w = tf.Variable(util.random_uniform(shape[0] * 2, shape[1]), dtype=tf.float32)
         self.b = tf.Variable(np.zeros((shape[0] * 2)), dtype=tf.float32)
@@ -18,13 +17,13 @@ class Layer:
         self.C = tf.Variable(np.zeros((shape[0] * 2, shape[0] * 2)), dtype=tf.float32)
         self.B = tf.Variable(np.zeros((shape[0] * 2)), dtype=tf.float32)
 
-    def create_forward_graph(self, input):
+    def create_forward_graph(self, input, learning_coeff):
         expanded = tf.reshape(mirroring_relus(input), [-1, 2 * self.shape[0]])
         biased = expanded - self.b
         reduced = tf.matmul(biased, self.w)
 
-        gather_B = tf.assign(self.B, tf.reduce_mean(expanded, axis=0) * (self.learning_coeff) + self.B * (1 - self.learning_coeff)).op
-        gather_C = tf.assign(self.C, tf.matmul(biased, biased, transpose_a=True) * (self.learning_coeff) + self.C * (1 - self.learning_coeff)).op
+        gather_B = tf.assign(self.B, tf.reduce_mean(expanded, axis=0) * (learning_coeff) + self.B * (1 - learning_coeff)).op
+        gather_C = tf.assign(self.C, tf.matmul(biased, biased, transpose_a=True) * (learning_coeff) + self.C * (1 - learning_coeff)).op
 
         learn_op_b = tf.assign(self.b, self.B).op
 
@@ -47,45 +46,46 @@ class Layer:
 
 class Autoencoder:
 
-    def __init__(self, sess, input_size, layer_sizes, learning_coeff, learning_rate=0.01):
+    def __init__(self, sess, input_size, layer_sizes, learning_rate=0.01):
         self.sess = sess
         self.input_size = input_size
         self.layer_sizes = layer_sizes
-        self.learning_coeff = learning_coeff
         self.layers = []
         self.debug = []
 
         s0_ = input_size[0] + input_size[1]
         for s1_ in layer_sizes:
-            self.layers.append(Layer((s0_, s1_), learning_coeff, learning_rate))
+            self.layers.append(Layer((s0_, s1_), learning_rate))
             s0_ = s1_
             self.debug.append(self.layers[len(self.layers) - 1].debug())
 
+        self.learning_coeff = tf.placeholder(tf.float32, ())
         self.gpu_inputs = tf.placeholder(tf.float32, [1, input_size[0]])
         self.gpu_labels = tf.placeholder(tf.float32, [1, input_size[1]])
 
-        _, collect_ops, learn_ops = self._create_forward_graph(self.gpu_inputs, self.gpu_labels)
+        self.info_space_start, collect_ops, learn_ops = self._create_forward_graph(self.gpu_inputs, self.gpu_labels, self.learning_coeff)
         self.collect_ops = collect_ops
         self.learn_ops = learn_ops
 
+        self.input_space = tf.Variable(np.zeros((1, input_size[0])), dtype=tf.float32)
         self.info_space = tf.Variable(np.zeros((1, s0_)), dtype=tf.float32)
-        self.info_space_start, _, __ = self._create_forward_graph(self.gpu_inputs, self.gpu_labels)
-        self.reset_ops = tf.assign(self.info_space, self.info_space_start).op
+        self.reset_ops = tf.assign(self.input_space, self.gpu_inputs).op
+        self.project_ops = tf.assign(self.info_space, self.info_space_start).op
 
         self.projected_input, self.projected_label = self._create_backward_graph(self.info_space)
-        infer_objective = tf.reduce_sum(tf.squared_difference(self.gpu_inputs, self.projected_input))
+        infer_objective = tf.reduce_sum(tf.squared_difference(self.input_space, self.projected_input))
         infer_grad = tf.gradients(infer_objective, [self.info_space])
         self.infer_ops = util.apply_gradients(zip(infer_grad, [self.info_space]), infer_objective, learning_rate)
 
         scope = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
         self.saver = tf.train.Saver(var_list=scope, keep_checkpoint_every_n_hours=1)
 
-    def _create_forward_graph(self, input, label):
+    def _create_forward_graph(self, input, label, learning_coeff):
         collect_ops = []
         learn_ops = []
         v = tf.concat([input, label], axis=1)
         for l in self.layers:
-            v, collect_op, learn_op = l.create_forward_graph(v)
+            v, collect_op, learn_op = l.create_forward_graph(v, learning_coeff)
             collect_ops.append(collect_op)
             learn_ops.append(learn_op)
         return v, collect_ops, learn_ops
@@ -96,17 +96,20 @@ class Autoencoder:
             h = l.create_backward_graph(h)
         return tf.slice(h, [0, 0], [-1, self.input_size[0]]), tf.slice(h, [0, self.input_size[0]], [-1, self.input_size[1]])
 
-    def collect(self, data, label):
-        self.sess.run(self.collect_ops, feed_dict={self.gpu_inputs: data, self.gpu_labels: label})
+    def collect(self, data, label, significance):
+        self.sess.run(self.collect_ops, feed_dict={self.gpu_inputs: data, self.gpu_labels: label, self.learning_coeff: significance})
 
-    def learn(self, data, label):
-        self.sess.run(self.learn_ops, feed_dict={self.gpu_inputs: data, self.gpu_labels: label})
+    def learn(self):
+        self.sess.run(self.learn_ops)
 
-    def reset_labels(self, data):
-        self.sess.run(self.reset_ops, feed_dict={self.gpu_inputs: data, self.gpu_labels: np.zeros((1, self.input_size[1]))})
+    def reset_input(self, data, label):
+        self.sess.run(self.reset_ops, feed_dict={self.gpu_inputs: data, self.gpu_labels: label})
 
-    def infer(self, data):
-        return self.sess.run((self.projected_label, self.projected_input, self.infer_ops), feed_dict={self.gpu_inputs: data})
+    def reset_info(self, data, label):
+        self.sess.run(self.project_ops, feed_dict={self.gpu_inputs: data, self.gpu_labels: label})
+
+    def infer(self):
+        return self.sess.run((self.projected_label, self.projected_input, self.infer_ops))
 
     def debug_test(self):
         print self.sess.run(self.debug)
